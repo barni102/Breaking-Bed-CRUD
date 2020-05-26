8350fe8d8d98e0914693cd06b1b2005bb836a89c
barni102 <kovacs102@gmail.com>
2020-05-26 16:46:07 +0200

    adat megjelenités bug kíjavítva

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -2,36 +2,38 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from '../services/data.service';
 import { Router } from '@angular/router';
 
-
-
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent implements OnInit {
   characters = [];
 
-  constructor(private service: DataService, private router: Router) { }
+  constructor(private service: DataService, private router: Router) {}
 
   ngOnInit(): void {
-      
-      
-      this.characters = this.service.getCharcacters();
+    let length = this.service.getCharcacters().length;
 
+    if (length == 0) {
+      setTimeout(() => {
+        this.characters = this.service.getCharcacters();
+      }, 1000);
+    } else {
+      this.characters = this.service.getCharcacters();
+    }
   }
-  removeCharacter(id : number): void {
-    this.service.removeCharacter(id)
+  removeCharacter(id: number): void {
+    this.service.removeCharacter(id);
   }
 
   refresh(): void {
     window.location.reload();
   }
-  goToDetails(charactersId: number){
-    this.router.navigate(["details/",charactersId]);
+  goToDetails(charactersId: number) {
+    this.router.navigate(['details/', charactersId]);
   }
-  addNew(){
-    this.router.navigate(["add"]);
+  addNew() {
+    this.router.navigate(['add']);
   }
-
 }
